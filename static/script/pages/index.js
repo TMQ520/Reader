be1820fa4276520f56cb477df462652a196bd787
tmq520 <1005275284@qq.com>
2017-12-05 15:09:41 +0800

    修改

--- static/script/pages/index.js
+++ static/script/pages/index.js
@@ -21,12 +21,19 @@ $.get('/ajax/index', function (d) {
 			topic: d.items[6].data.data,
 			duration: 0,
 			position: 0,
-			header_position: 0,
+			header_position: index_header_tab_width/2,
 			header_duration: 0,
 			tab_1_class: 'Swipe-tab_on',
 			tab_2_class: ''
 		},
-
+		created: function(){
+			$(window).on('resize',function(){
+				window.location.reload();
+				/*if($(this).width() < 768){
+					window.location.reload();
+				}*/
+			})
+		},
 		//事件绑定---书城和书架的切换
 		methods: {
 			tabSwitch: function (pos) {
@@ -34,12 +41,12 @@ $.get('/ajax/index', function (d) {
 				this.header_duration = 0.5;
 				if(pos == 0) {
 					this.position = 0;
-					this.header_position = 0;
+					this.header_position = index_header_tab_width/2;
 					this.tab_1_class = "Swipe-tab_on";
 					this.tab_2_class = "";
 				} else {
 					this.position = (-windowWidth);
-					this.header_position = index_header_tab_width;
+					this.header_position = index_header_tab_width/2 + index_header_tab_width;
 					this.tab_2_class = "Swipe-tab_on";
 					this.tab_1_class = "";
 				}
